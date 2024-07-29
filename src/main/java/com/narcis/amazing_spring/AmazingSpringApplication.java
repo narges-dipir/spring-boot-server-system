1f4c445a93ec1072453ccab138cadb4bb90815cd
narges dipir <nargesdeypir@gmail.com>
2024-07-29 21:28:43 +0200

    initialization

---
+++ src/main/java/com/narcis/amazing_spring/AmazingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.narcis.amazing_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narcis/amazing_spring/AmazingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narcis.amazing_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
