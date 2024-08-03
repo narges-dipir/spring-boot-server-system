49f47a8b33e6299d5297056ed05feab6058bb371
narges dipir <nargesdeypir@gmail.com>
2024-08-03 07:48:02 +0200

    coffee class

--- src/main/java/com/narcis/amazing_spring/AmazingSpringApplication.java
+++ src/main/java/com/narcis/amazing_spring/AmazingSpringApplication.java
@@ -3,6 +3,8 @@ package com.narcis.amazing_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class AmazingSpringApplication {
 
@@ -10,4 +12,28 @@ public class AmazingSpringApplication {
 		SpringApplication.run(AmazingSpringApplication.class, args);
 	}
 
+}
+class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
